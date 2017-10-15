ed8ce73fac1b0aa758462f1b8c0ae3fbe20058fe
yuizho <yuizho3@gmail.com>
2017-10-15 22:56:31 +0900

    add some webpack configs

--- src/main/resources/src/scripts/index.js
+++ src/main/resources/src/scripts/index.js
@@ -1,11 +1,20 @@
 import _ from 'lodash';
+import printMe from './print.js';
+import '../css/style.css'
+import '../css/milligram.min.css'
 
 function component() {
-  var element = document.createElement('div');
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
-  return element;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
+    return element;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
-  entry: './src/main/resources/src/scripts/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'src/main/resources/static/scripts')
-  }
+    entry: {
+        hello: './src/main/resources/src/scripts/index.js',
+        print: './src/main/resources/src/scripts/print.js'
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+      new CleanWebpackPlugin(['src/main/resources/static/scripts'])
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'src/main/resources/static/scripts')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
 };
